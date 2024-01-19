0adffe736aa319490de186c149d975d0d31daab5
Jared Winter <jwinternet@outlook.com>
2024-01-19 15:07:36 -0500

    Completed Chapter 1!

--- my_cool_site/settings.py
+++ my_cool_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_blog.apps.MyBlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_cool_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'my_blog/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
